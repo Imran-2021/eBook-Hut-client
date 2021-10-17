c3dcdf74ce41210ab4ea654454f6befc87d5284b
Imran-2021 <mdimranulhaque.8dec20@gmail.com>
2021-10-17 22:20:56 +0600

    more design cart

--- src/Components/Carts/CartItems.js
+++ src/Components/Carts/CartItems.js
@@ -22,6 +22,7 @@ const CartItems = () => {
                     <p className="h6 cursor"><FaTrashAlt/></p>
                 </div>
             </div>
+            <hr />
             <div className="cart-container m-auto">
                 <div className="incressDecress">
                     <img className="imgg" width="90" height="90"src="https://i.ibb.co/VJ3VW6X/book.png" alt="" />
@@ -41,44 +42,7 @@ const CartItems = () => {
                     <p className="h6 cursor"><FaTrashAlt/></p>
                 </div>
             </div>
-            <div className="cart-container m-auto">
-                <div className="incressDecress">
-                    <img className="imgg" width="90" height="90"src="https://i.ibb.co/VJ3VW6X/book.png" alt="" />
-                </div>
-                <div className="incressDecress">
-                    <h5>Book 2</h5>
-                </div>
-                <div className="incressDecress h5" >
-                    <span className="cursor"><FaMinus/></span> 
-                    <input className="inputs mx-2" id="carts" type="text" placeholder="1" readOnly/>
-                    <span className="cursor"><FaPlus/></span>
-                </div>
-                <div className="incressDecress ">
-                    <h3 className="h5">$<span>20</span></h3>
-                </div>
-                <div className="incressDecress">
-                    <p className="h6 cursor"><FaTrashAlt/></p>
-                </div>
-            </div>
-            <div className="cart-container m-auto">
-                <div className="incressDecress">
-                    <img className="imgg" width="90" height="90"src="https://i.ibb.co/VJ3VW6X/book.png" alt="" />
-                </div>
-                <div className="incressDecress">
-                    <h5>Book 2</h5>
-                </div>
-                <div className="incressDecress h5" >
-                    <span className="cursor"><FaMinus/></span> 
-                    <input className="inputs mx-2" id="carts" type="text" placeholder="1" readOnly/>
-                    <span className="cursor"><FaPlus/></span>
-                </div>
-                <div className="incressDecress ">
-                    <h3 className="h5">$<span>20</span></h3>
-                </div>
-                <div className="incressDecress">
-                    <p className="h6 cursor"><FaTrashAlt/></p>
-                </div>
-            </div>
+            <hr />
             <div className="cart-container m-auto">
                 <div className="incressDecress">
                     <img className="imgg" width="90" height="90"src="https://i.ibb.co/VJ3VW6X/book.png" alt="" />
