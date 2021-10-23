deea08b401563f4d97e2d8e88b34eec9cf25a614
Imran-2021 <mdimranulhaque.8dec20@gmail.com>
2021-10-23 12:20:19 +0600

    add modal on cart section done

--- src/Components/Carts/Carts.js
+++ src/Components/Carts/Carts.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 import "./Carts.css"
 import { FaPlus, FaMinus, FaTrashAlt } from "react-icons/fa";
 import CartItems from './CartItems';
+import { Modal,Button } from 'react-bootstrap';
 
 const Carts = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+    const namee= "md imranul haque"
     return (
+        <>
         <div className="container">
             <p className="pt-1 ctn"> <Link className="text-linkr" to="/books"><FaArrowLeft /> Continue Book shopping</Link> </p>
             <div>
@@ -21,10 +25,43 @@ const Carts = () => {
             </div>
             <div className="text-end px-5">
                 <h5 className="me-3">Cart Total : - $<span>32513</span></h5>
-                <button className="btn btn-primary w-25 ">CheckOut</button>
+                <button onClick={() => setModalShow(true)}  className="btn btn-primary w-25 ">CheckOut</button>
             </div>
         </div>
+        <MyVerticallyCenteredModal
+        show={modalShow}
+        namee={namee}
+        onHide={() => setModalShow(false)}
+      />
+        </>
     );
 };
 
 export default Carts;
+function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Happy buying
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>your History</h4>
+          <h3>{props.namee}</h3>
+          <p>You have 7 items in cart</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad pariatur quos optio. Excepturi, sint, atque id vero, rem nemo natus vitae tempora voluptate incidunt quidem cum. Amet, repudiandae. At, quaerat.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+          <Button onClick={props.onHide}>buy Now</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+  
